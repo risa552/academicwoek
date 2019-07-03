a8768cfde4f68140469f3894a9c904277ca0df5c
Risa5522 <luvexoka@gmail.com>
2019-07-03 11:26:46 +0700

    การสร้างโมดูล

---
+++ app/Modules/ModuleServiceProvider.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\_backoffice;
+
+use Illuminate\Support\ServiceProvider;
+
+class ModuleServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+}

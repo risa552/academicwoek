906ed1f32f2e3429b4e4399f658bcf11b70a82d0
Risa5522 <luvexoka@gmail.com>
2019-07-09 14:33:22 +0700

    แก้ไขเออเร่อ

--- app/Modules/routes.php
+++ app/Modules/routes.php
@@ -1,10 +1,7 @@
 <?php
   Route::Get('/','\App\Modules\Home\HomeController@index');
   Route::Get('/Login','\App\Modules\Login\LoginController@index');
-<<<<<<< HEAD
   Route::Get('/program','\App\Modules\Program\ProgramController@index');
-=======
   Route::Get('/Course','\App\Modules\Course\CourseController@index');
   Route::Get('/Test','\App\Modules\Test\TestController@index');
->>>>>>> manubar
 ?>

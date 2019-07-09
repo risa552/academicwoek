df2547f5514b3d15b47810a51237f5a7919c4e47
Risa5522 <luvexoka@gmail.com>
2019-07-09 17:03:12 +0700

    - ออกแบบหน้าส่งข้อสอบ - หน้าเพิ่มการส่งข้อสอบ

--- app/Modules/routes.php
+++ app/Modules/routes.php
@@ -4,4 +4,6 @@
   Route::Get('/program','\App\Modules\Program\ProgramController@index');
   Route::Get('/course','\App\Modules\Course\CourseController@index');
   Route::Get('/exam','\App\Modules\Exam\ExamController@index');
+  Route::Get('/exam/from','\App\Modules\Exam\ExamController@from');
+
 ?>

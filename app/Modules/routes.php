faa973d7193cf20b1b92b8285b278801078008d5
Risa5522 <luvexoka@gmail.com>
2019-07-10 10:59:35 +0700

    -ทำหน้าการเพิ่มการส่งข้อสอบ - การแจ้งเตือนการลบ

--- app/Modules/routes.php
+++ app/Modules/routes.php
@@ -6,4 +6,5 @@
   Route::Get('/exam','\App\Modules\Exam\ExamController@index');
   Route::Get('/exam/from','\App\Modules\Exam\ExamController@from');
 
+  Route::Get('/exam/edit','\App\Modules\Exam\ExamController@edit');
 ?>

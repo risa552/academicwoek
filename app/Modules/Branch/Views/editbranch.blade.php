eb1ae8e44386aa2fb152d536a6653120e840ca32
fanpatl <fan4949@gmail.com>
2019-07-12 16:59:16 +0700

    แก้หน้าสาขาวิชา

--- app/Modules/Branch/Views/editbranch.blade.php
+++ app/Modules/Branch/Views/editbranch.blade.php
@@ -19,6 +19,18 @@
                         <input type="password" class="form-control" id="pwd" placeholder="Enter password">
                         </div>
                     </div>
+                    <div class="form-group">
+                        <label class="control-label col-sm-2" for="pwd">รหัสหลักสูตร:</label>
+                        <div class="col-sm-10"> 
+                        <input type="password" class="form-control" id="pwd" placeholder="Enter password">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="control-label col-sm-2" for="pwd">รหัสกลุ่มเรียน:</label>
+                        <div class="col-sm-10"> 
+                        <input type="password" class="form-control" id="pwd" placeholder="Enter password">
+                        </div>
+                    </div>
                     <div class="form-group" > 
                         <div class="col-sm-offset-2 col-sm-10">
                             <button type="submit" class="btn btn-secondary"><a  href="/branch" class="fa fa-arrow-left" aria-hidden="true"> back</a></button>

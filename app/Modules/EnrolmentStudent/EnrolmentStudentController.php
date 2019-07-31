8ef7f62fc877ddc480d4a2f8205a03c17b548918
Risa5522 <luvexoka@gmail.com>
2019-07-31 14:02:17 +0700

    ทำหน้าลงทะเบียนนักเรียน

--- app/Modules/EnrolmentStudent/EnrolmentStudentController.php
+++ app/Modules/EnrolmentStudent/EnrolmentStudentController.php
@@ -12,6 +12,12 @@ class EnrolmentStudentController extends Controller
 {
     public function index()
     {
+        $items = DB::table('enrolment')
+        ->select('enrolment.*','student.std_fname','program.program_name')
+        ->leftJoin('student','enrolment.std_id','student.std_id')
+        ->leftJoin('program','enrolment.program_id','program.program_id')
+        ->whereNull('enrolment.delete_at');
+
         return view ('enrostudent::list');
     }
 }

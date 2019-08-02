f03c94a669ce43952444ffcd59d7d6fd161a5f55
fanpatl <fan4949@gmail.com>
2019-08-02 16:03:32 +0700

    ทำออกเกรด เรียบร้อย

--- app/Modules/Grade/GradeController.php
+++ app/Modules/Grade/GradeController.php
@@ -11,8 +11,47 @@ use App\Services\CurrentUser;
 
 class GradeController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-    return view('grade::form');
+        $keyword = $request->get('keyword');
+        $term_id = $request->get('term_id');
+
+        $user=CurrentUser::user();
+        $grade = DB::table('program')
+        ->select('program.program_id',
+        'subject.sub_code',
+        'subject.sub_name',
+        'student.first_name',
+        'student.last_name',
+        'enrolment.grade')
+        ->leftJoin('subject','program.sub_id','subject.sub_id')
+        ->leftJoin('enrolment','enrolment.program_id','program.program_id')
+        ->rightJoin('student','enrolment.std_id','student.std_id')
+    
+        ->where('program.teach_id',$user->teach_id)
+        ->whereExists(function ($query) {
+            $query->select(DB::raw(1))
+                  ->from('term')
+                  ->where('startdate','<=',date('Y-m-d'))
+                  ->where('enddate','>=',date('Y-m-d'))
+                  ->whereRaw('program.term_id = term.term_id');
+        })
+        ->whereNull('program.delete_at')
+        ->whereNull('student.delete_at')
+        ->whereNull('enrolment.delete_at');
+
+        if(!empty($keyword)){
+            $grade->where(function ($query) use($keyword){
+                $query->where('term_id','LIKE','%'.$keyword.'%');
+            });
+        }
+        if(is_numeric($term_id))
+        {
+            $grade->where('program.term_id','=',$term_id);
+        }
+         // print_r($grade);exit;
+        $grade = $grade->get();
+        $rom = DB::table('term')->whereNull('delete_at')->get();
+        return view('grade::form',compact('grade','rom'));
     }
 }
--- app/Modules/Grade/Views/form.blade.php
+++ app/Modules/Grade/Views/form.blade.php
@@ -1,36 +1,65 @@
 @extends('academic-layout') 
-@section('title','ข้อมูลออกเกรด')
+@section('title','ออกเกรด')
 @section('content')
-div class="container">
+<div class="container">
     <div class="row">
-        <div class="col-md-10">
-            <div class="panel panel-default">  
-                <div class="panel-heading">
-                ออกเกรด
+        <div class="col-md-3">
+            <div class="panel panel-default">
+                <div class="panel-heading">ค้นหาข้อมูลภาคเรียน</div>
+                <div class="panel-body">
+                    <form action="/grade">
+                    <div class="form-group">
+                            <label >ภาคเรียน:</label>
+                            <select name="term_id">
+                                <option value="all">
+                                    ทั้งหมด
+                                </option>
+                            @foreach($rom as $index => $row2)
+                                <option value ="{{$row2->term_id}}" {{Input::get('term_id')==$row2->term_id?'selected':''}}>
+                                    {{$row2->term_name}}
+                                </option>
+                            @endforeach
+                            </select>
+                    </div>
+                        <button type="submit" class="btn btn-default">ยืนยัน</button>
+                    </form>
                 </div>
+            </div>
+            <!--<button type="submit" class="btn btn-info"><a href="#">อาจารย์</a></button> -->
+        </div> 
+
+        <div class="col-md-9">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                รายการการออกเกรด
+                </div>
+                <div class="panel-body">
                     <table class="table table-striped">
                         <thead>
                             <tr>
                                 <th>วิชา</th>
                                 <th>นักศึกษา</th>
                                 <th>เกรด</th>
-                                <th style="width:110px">แก้ไขรายการ</th>
                             </tr>
                         </thead>
                         <tbody>
                         @foreach($grade as $index => $row)
                             <tr>
-                                <td>{{$row->sub_name}}</td>
+                                <td>{{$row->sub_code}}</td>
                                 <td>{{$row->first_name}} {{$row->last_name}}</td>
-                                <td></td>
                                 <td>
+                                <input type="text" name="keyword"}}/>
                                 </td>
                             </tr>
                         @endforeach
                         </tbody>
                     </table>
-     @endsection
+                    <button class="bth" style="margin-left:700px; margin-bottom:10px;"> <i class="fa fa-check" aria-hidden="true"> ยืนยัน</i></button>
+                </div>
+            </div>
+        </div>
+        
+    </div>  
+</div>
 
-
-
-                   
+@endsection
